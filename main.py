8db41d7d876a416b6e224df129a22d7a553c8752
RyotaroAbe <ryotaroynwa1225@gmial.com>
2022-08-30 15:00:08 +0000

    initial commit

--- main.py
+++ main.py
@@ -10,23 +10,49 @@ favicon_path = './book-stack.png'
 
 DATABASE_URL = os.getenv('DATABASE_URL', None)
 print(DATABASE_URL)
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-cur = conn.cursor()
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/db")
+@app.get("/users")
 def get_data():
-    query = "SELECT * FROM Users"
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    cur = conn.cursor()
+    table_name = "Users"
+    query = f"SELECT * FROM {table_name}"
     cur.execute(query)
-    result = cur.fetchone()
+    result = cur.fetchall()
     print(result)
     cur.close()
     conn.close()
+    return {"data": f'{result}'}
 
+@app.get("/stacks")
+def get_data():
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    cur = conn.cursor()
+    table_name = "Stacks"
+    query = f"SELECT * FROM {table_name}"
+    cur.execute(query)
+    result = cur.fetchall()
+    print(result)
+    cur.close()
+    conn.close()
+    return {"data": f'{result}'}
+
+@app.get("/books")
+def get_data():
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    cur = conn.cursor()
+    table_name = "Books"
+    query = f"SELECT * FROM {table_name}"
+    cur.execute(query)
+    result = cur.fetchall()
+    print(result)
+    cur.close()
+    conn.close()
     return {"data": f'{result}'}
 
 @app.get("/favicon.ico")
