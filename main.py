594ff7a25909723862c1e34242649e53703e7f95
RyotaroAbe <ryotaroynwa1225@gmial.com>
2022-08-30 14:28:34 +0000

    initial commit

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 
 app = FastAPI()
-
+# favicon_path = 'favicon.ico'
+favicon_path = './book-stack.png'
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/favicon.ico")
+def favicon():
+    return FileResponse(favicon_path)
