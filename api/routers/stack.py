8401506ca68fdb7519f4ef4f190ba0eceb7e3969
RyotaroAbe45123 <ryotaroynwa1225@gmail.com>
2022-09-18 13:13:51 +0000

    update: add header for token to stakcs

--- api/routers/stack.py
+++ api/routers/stack.py
@@ -11,7 +11,6 @@ router = APIRouter()
 
 
 @router.get("/stacks", response_model=Union[List[schema.Stack], List[None]])
-# async def read_stacks(user_id: int):
 async def read_stacks(token: str = Header(default=None)):
     user_id = get_user_info(token)
     # from datetime import datetime
@@ -20,11 +19,8 @@ async def read_stacks(token: str = Header(default=None)):
 
 
 @router.post("/stack", response_model=schema.StackCreateResponse)
-# async def create_stack(body: schema.StackCreate, token: Optional[str] = Header(default=None)):
 async def create_stack(body: schema.StackCreate, token: str = Header(default=None)):
     user_id = get_user_info(token)
-    print(user_id)
-    # user_id = long(user_id)
     print(type(user_id))
     # from datetime import datetime
     # return schema.StackCreateResponse(user_id=user_id, **body.dict(), timestamp=datetime.now())
@@ -32,9 +28,10 @@ async def create_stack(body: schema.StackCreate, token: str = Header(default=Non
     
     
 @router.delete("/stack", response_model=None)
-async def delete_stack(body: schema.StackDelete):
-    stack = await crud.read_stack(body)
+async def delete_stack(body: schema.StackDelete, token: str = Header(default=None)):
+    user_id = get_user_info(token)
+    stack = await crud.read_stack(user_id, body)
     if stack is None:
         raise HTTPException(status_code=404, detail="Stack Not Found")
     # return None
-    return await crud.delete_stack(body)
+    return await crud.delete_stack(user_id, body)
