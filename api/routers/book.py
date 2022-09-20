db612a46bbdecd55a1ecc723e07d641a6733ae5d
RyotaroAbe45123 <ryotaroynwa1225@gmail.com>
2022-09-20 15:37:02 +0000

    fix: remove api router

--- api/routers/book.py
+++ api/routers/book.py
@@ -9,20 +9,20 @@ import api.cruds.book as crud
 router = APIRouter()
 
 
-@router.get("/book", response_model=Union[schema.Book, None])
+# @router.get("/book", response_model=Union[schema.Book, None])
 # async def read_book(body: schema.BookRead):
 async def read_book(isbn: int):
     # return schema.Book(**body.dict(), author="author1", publisher="publisher1", title="title1")
     return await crud.read_book(isbn)
 
 
-@router.post("/book", response_model=schema.BookCreateResponse)
+# @router.post("/book", response_model=schema.BookCreateResponse)
 async def create_book(body: schema.BookCreate):
     # return schema.BookCreateResponse(**body.dict())
     return await crud.create_book(body)
     
     
-@router.delete("/book", response_model=None)
+# @router.delete("/book", response_model=None)
 async def delete_book(body: schema.BookDelete):
     book = await crud.read_book(body.isbn)
     if book is None:
