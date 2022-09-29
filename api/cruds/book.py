35107531e708921be06c327bad1d6528854ea595
RyotaroAbe45123 <ryotaroynwa1225@gmail.com>
2022-09-29 13:26:15 +0000

    fix: remove comment out

--- api/cruds/book.py
+++ api/cruds/book.py
@@ -7,20 +7,23 @@ from ..database import get_async_connection
 import api.schemas.book as schema
 
 
-async def read_all_books(user_id: int) -> Union[List[schema.BookRead], List[None]]:
+async def read_all_books(user_id: int) -> Union[List[schema.BooksReadResponse], List[None]]:
     async with await get_async_connection() as aconn:
-        async with aconn.cursor(row_factory=class_row(schema.BookRead)) as acur:
+        async with aconn.cursor(row_factory=class_row(schema.BooksReadResponse)) as acur:
             await acur.execute(
-                "SELECT * FROM Stacks AS S LEFT JOIN Books AS B ON S.ISBN = B.ISBN WHERE S.UserId = %s",
+                "SELECT S.isbn, author, publisher, title, price, pages \
+                    FROM Stacks AS S \
+                        LEFT JOIN Books AS B ON S.ISBN = B.ISBN \
+                            WHERE S.UserId = %s",
                 (user_id,)
             )
             obj = await acur.fetchall()
             return obj if obj else [None]
 
 
-async def read_book(isbn: int) -> Union[schema.BookRead, None]:
+async def read_book(isbn: int) -> Union[schema.BookReadResponse, None]:
     async with await get_async_connection() as aconn:
-        async with aconn.cursor(row_factory=class_row(schema.BookRead)) as acur:
+        async with aconn.cursor(row_factory=class_row(schema.BookReadResponse)) as acur:
             await acur.execute(
                 "SELECT * FROM Books WHERE Books.ISBN = %s",
                 (isbn,)
@@ -39,12 +42,3 @@ async def create_book(body: schema.BookCreate) -> schema.BookCreateResponse:
                 (body.isbn, body.author, body.publisher, body.title, body.price, body.pages)
             )
             return body
-
-
-# async def delete_book(body: schema.BookDelete) -> None:
-#     async with await get_async_connection() as aconn:
-#         async with aconn.cursor() as acur:
-#             await acur.execute(
-#                 "DELETE FROM Books WHERE Books.ISBN = %s",
-#                 (body.isbn,)
-#             )
--- api/routers/book.py
+++ api/routers/book.py
@@ -10,30 +10,19 @@ from ..utils import get_user_info
 router = APIRouter()
 
 
-@router.get("/books", tags=["book"], response_model=Union[List[schema.BookRead], List[None]])
+@router.get("/books", tags=["book"], response_model=Union[List[schema.BooksReadResponse], List[None]])
 async def read_books(token: str = Header(default=None)):
     user_id = get_user_info(token)
     # return schema.Book(**body.dict(), author="author1", publisher="publisher1", title="title1")
     return await crud.read_all_books(user_id)
     
 
-# @router.get("/book", tags=["book"], response_model=Union[schema.Book, None])
-async def read_book(isbn: int, token: str = Header(default=None)) -> Union[schema.BookRead, None]:
+async def read_book(isbn: int, token: str = Header(default=None)) -> Union[schema.BookReadResponse, None]:
     _ = get_user_info(token)
     # return schema.Book(**body.dict(), author="author1", publisher="publisher1", title="title1")
     return await crud.read_book(isbn)
 
 
-# @router.post("/book", response_model=schema.BookCreateResponse)
 async def create_book(body: schema.BookCreate):
     # return schema.BookCreateResponse(**body.dict())
     return await crud.create_book(body)
-    
-    
-# @router.delete("/book", response_model=None)
-# async def delete_book(body: schema.BookDelete):
-#     book = await crud.read_book(body.isbn)
-#     if book is None:
-#         raise HTTPException(status_code=404, detail="Book Not Found")
-#     # return None
-#     return await crud.delete_book(body)
