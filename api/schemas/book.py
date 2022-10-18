50ac5974fdfc1299369f4b492b61f62cea323332
RyotaroAbe45123 <ryotaroynwa1225@gmail.com>
2022-10-18 14:21:07 +0000

    update: change BookCreate and BookReadResponse

--- api/schemas/book.py
+++ api/schemas/book.py
@@ -18,24 +18,19 @@ class AllBooksReadResponse(BaseModel):
     data: dict
 
 
-class BookReadResponse(BookBase):
-    price: int
-    pages: int
-    author: str
-    publisher: str
-    title: str
-    subjects: Union[List[str], None]
-
-
 class BookCreate(BookBase):
     title: str
     pages: int
     c_code: int
-    category_code: int
+    category_code: Union[int, None]
     has_image: bool
     publisher: str
     publish_date: date
     price: int
+
+class BookReadResponse(BookCreate):
+    authors: Union[List[str], None]
+    subjects: Union[List[str], None]
 
 
 class BookCreateResponse(BookCreate):
