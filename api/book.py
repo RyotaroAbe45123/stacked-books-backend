71d27dd69ef75117c62be52173972288cc82574b
RyotaroAbe45123 <ryotaroynwa1225@gmail.com>
2022-10-18 16:28:54 +0000

    update: remove space

--- api/book.py
+++ api/book.py
@@ -31,22 +31,32 @@ class Book:
             subject_list = subject_list[0].split(";")
         return [subject for subject in subject_list]
 
+    def remove_space(self, keyword: str) -> str:
+        return "".join(keyword.split())
 
     def extract_info(self, data: dict):
         self.title = data["onix"]["DescriptiveDetail"]["TitleDetail"]["TitleElement"]["TitleText"]["content"]
+        self.title = self.remove_space(self.title)
 
         self.authors = data["onix"]["DescriptiveDetail"]["Contributor"]
         self.authors = [author["PersonName"]["content"] for author in self.authors]
-        self.authors = ["".join(author.split()) for author in self.authors]
+        self.authors = [self.remove_space(author) for author in self.authors]
 
-        self.pages = data["onix"]["DescriptiveDetail"]["Extent"][0]["ExtentValue"]
+        try:
+            self.pages = data["onix"]["DescriptiveDetail"]["Extent"][0]["ExtentValue"]
+        except KeyError:
+            self.pages = 0
 
         for i, j in enumerate(data["onix"]["DescriptiveDetail"]["Subject"]):
-            if i == 0:
-                self.c_code = j["SubjectCode"]
-            elif i == 1:
-                self.category_code = j["SubjectCode"]
-            elif i == 2:
+            try:
+                code = j["SubjectCode"]
+                if (len(code) == 4):
+                    self.c_code = j["SubjectCode"]
+                elif (len(code) == 2):
+                    self.category_code = j["SubjectCode"]
+                else:
+                    print("key Error")
+            except KeyError:
                 self.subjects = j["SubjectHeadingText"]
                 self.subjects = self.search_subjects(self.subjects)
 
@@ -56,6 +66,7 @@ class Book:
             self.has_image = False
 
         self.publisher = data["onix"]["PublishingDetail"]["Imprint"]["ImprintName"]
+        self.publisher = self.remove_space(self.publisher)
         self.publish_date = data["onix"]["PublishingDetail"]["PublishingDate"][0]["Date"]
 
         self.price = data["onix"]["ProductSupply"]["SupplyDetail"]["Price"][0]["PriceAmount"]
