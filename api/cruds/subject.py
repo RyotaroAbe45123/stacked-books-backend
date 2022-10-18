76ebbd95ab7ccc0e1ef18c7df440f4c7a485e78f
RyotaroAbe45123 <ryotaroynwa1225@gmail.com>
2022-10-18 14:26:29 +0000

    fix: remove comment out

--- api/cruds/subject.py
+++ api/cruds/subject.py
@@ -8,7 +8,6 @@ from ..database import get_async_connection
 
 async def read_subjects(isbn: int) -> Union[List[schema.SubjectBase], List[None]]:
     async with await get_async_connection() as aconn:
-        # async with aconn.cursor() as acur:
         async with aconn.cursor(row_factory=class_row(schema.SubjectBase)) as acur:
             await acur.execute(
                 "SELECT subject FROM Subjects Where Subjects.ISBN = %s",
@@ -18,11 +17,10 @@ async def read_subjects(isbn: int) -> Union[List[schema.SubjectBase], List[None]
             return obj
 
 
-async def create_subject(isbn: int, subject: str):
+async def create_subject(isbn: int, keyword: str) -> None:
     async with await get_async_connection() as aconn:
         async with aconn.cursor() as acur:
-        # async with aconn.cursor(row_factory=class_row(schema.BookCreateResponse)) as acur:
             await acur.execute(
                 "INSERT INTO Subjects VALUES (%s, %s)",
-                (isbn, subject)
+                (isbn, keyword)
             )
