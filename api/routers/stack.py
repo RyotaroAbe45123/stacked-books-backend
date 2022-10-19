6fdb2e9b264dd1d1c896a63ed64584c4ec6a29fa
RyotaroAbe45123 <ryotaroynwa1225@gmail.com>
2022-10-19 11:08:17 +0000

    updaate: schema crud

--- api/routers/stack.py
+++ api/routers/stack.py
@@ -7,6 +7,7 @@ import api.schemas.stack as schema
 import api.cruds.stack as crud_stack
 import api.cruds.book as crud_book
 import api.cruds.author as crud_author
+# import api.cruds.subject as crud_subject
 from api.utils import get_user_info
 from api.book import Book
 
@@ -14,7 +15,7 @@ from api.book import Book
 router = APIRouter()
 
 
-@router.get("/stacks", tags=["stack"], response_model=Union[List[schema.StacksReadResponse], List[None]])
+@router.get("/stacks", tags=["stack"], response_model=Union[List[schema.StacksReadResponse], List])
 async def read_stacks(token: str = Header(default=None)):
     user_id = get_user_info(token, os.getenv("IS_LOCAL"))
     return await crud_stack.read_all_stacks(user_id)
@@ -39,6 +40,8 @@ async def create_stack(body: schema.StackCreate, token: str = Header(default=Non
             await crud_book.create_book(book)
             for author in book.authors:
                 await crud_author.create_author(book.isbn, author)            
+            # for subject in book.subjects:
+            #     await crud_subject.create_subject(book.isbn, subject)            
         except HTTPException as e:
             raise e
     return await crud_stack.create_stack(user_id, body)
