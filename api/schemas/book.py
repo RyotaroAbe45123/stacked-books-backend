941ae4f7f8528c6573aad4fe6c8be96646b1a191
RyotaroAbe45123 <ryotaroynwa1225@gmail.com>
2022-11-06 09:35:36 +0000

    update: book api

--- api/schemas/book.py
+++ api/schemas/book.py
@@ -6,33 +6,25 @@ from typing import List, Union
 class BookBase(BaseModel):
     isbn: int
 
-
-class BooksReadResponse(BookBase):
-    price: Union[int, None]
-    pages: Union[int, None]
-    c_code: Union[str, None]
-    publisher: str
-    title: str
-    has_image: bool
-
 class AllBooksReadResponse(BaseModel):
     data: dict
 
-
 class BookCreate(BookBase):
     title: str
-    pages: int
+    pages: Union[int, None]
     c_code: Union[str, None]
     category_code: Union[str, None]
     has_image: bool
     publisher: str
     publish_date: date
-    price: int
+    price: Union[int, None]
+
+class BooksReadResponse(BookCreate):
+    pass
 
 class BookReadResponse(BookCreate):
     authors: Union[List[str], None]
     # subjects: Union[List[str], None]
-
 
 class BookCreateResponse(BookCreate):
     pass
